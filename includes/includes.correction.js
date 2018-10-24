94be043727ac1a7df7ee56584f400311c12f1a74
Kristen Garnier <kristengarnier@MacBook-Pro-de-Kristen.local>
2018-10-24 23:41:59 +0200

    Add includes exercices

---
+++ includes/includes.correction.js
@@ -0,0 +1,20 @@
+// Reformattez ces exemple en utilisant include à la place de indexOf
+
+const geraltAndThegirls = ['Ciri', 'Geralt of Rivia', 'Triss Merigold', 'Yennefer']
+
+if(geraltAndThegirls.indexOf('Geralt of Rivia') >= 0) {
+    console.log('Good you found Geralt')
+}
+
+const doesItContainsCat = ['Dog', 'Cat', 'Fish']
+
+if(doesItContainsCat.indexOf('Cat') >= 0) {
+    console.log('What a suprize!')
+}
+
+
+// Checkez si le mot 'Hogwarts' est inclut dans cette dans cette phrase
+const ItAlsoWorkOnString = 'I can’t believe we’re at Hogwarts! No, that’s Buckingham Palace.'
+
+// Cherchez si dans se mot, 'ca' existe
+const doesNotExistsInThere = 'I really only listen to german death reggae'
