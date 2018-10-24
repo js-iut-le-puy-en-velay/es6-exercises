b5480c0096d12642db8fb7e3408d63645a733785
Kristen Garnier <kristengarnier@MacBook-Pro-de-Kristen.local>
2018-10-24 23:41:23 +0200

    Arrow exercices

---
+++ arrow-function/arrow.correction.js
@@ -0,0 +1,12 @@
+// Remplacer les fonctions par des arrow functions
+
+function sayHello(name) {
+    return 'Hello ' + name;
+}
+
+const addition = function(a, b) {
+    return a + b;
+}
+
+console.log(sayHello('Kristen'))
+console.log(addition(1, 2))
