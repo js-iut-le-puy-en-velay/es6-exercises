d22eef966f332747db72124dfc517b4f5c269d73
Kristen Garnier <kristengarnier@MacBook-Pro-de-Kristen.local>
2018-10-24 23:42:09 +0200

    Add objet assignment exercices

---
+++ object-propert-assignment/object-assignement.js
@@ -0,0 +1,34 @@
+// Ajouter la propriété "salade" à l'objet ci dessous
+
+const amIAFruit = {
+    tomato: true,
+    eggplant: true,
+    cilitbang: false,
+    jasonstatham: false
+}
+
+// Fusionnez les objets ci dessous pour n'en faire plus qu'un seul
+
+const autaumnSalad = {
+    better: 'better',
+    call: 'call',
+    it: 'it',
+    soup: 'soup'
+} 
+
+const ninjaTurtles = {
+    red: 'Raphael',
+    orange: 'Michaelangello',
+    blue: 'Leonardo',
+    purple: 'Tagiatello'
+}
+
+// remplacer une propriété de l'objet ci dessous
+
+const dragonball = {
+    goku: 9001,
+    vegeta: 8999,
+    bumla: 3,
+    krilin: 2200,
+    'Jackie Chun': 139 
+}
