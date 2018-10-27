d80c6bbb271d8edf7cea8c4adaf031106992e91f
Kristen Garnier <kristengarnier@MacBook-Pro-de-Kristen.local>
2018-10-27 13:33:38 +0200

    Add instructions

---
+++ require/require.js
@@ -0,0 +1,6 @@
+// Dans un nouveau fichier, créez une classe simple et exportez la (Vous pouvez utiliser Calculator3001 pour la classe)
+// Importez la ensuite dans ce fichier via require
+// Utilisez ensuite la classe au sein du fichier
+// Installez le packet npm suivant au sein du dossier : https://www.npmjs.com/package/starwars
+// Ensuite importez le packet dans ce script
+// Utilisez le pour afficher des citations
